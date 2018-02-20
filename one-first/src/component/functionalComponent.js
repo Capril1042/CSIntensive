a20de74572d1da5bc5c302cd36f7fba9b3bad3fd
Capril1042 <Caprilewis1042@gmail.com>
2018-02-20 14:49:40 -0500

    commit one and two

---
+++ one-first/src/component/functionalComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import React, { Component } from 'react';
+
+const FunctionalComponent =() =>
+{
+return (
+    <div>
+       <h1> Functional Component </h1>
+        </div>
+)
+}
+export default FunctionalComponent;
